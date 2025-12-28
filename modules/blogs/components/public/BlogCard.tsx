ab8a50574eaa17b1ff018b07538a678f7c7075b5
Tareq <md.tareq@asthait.com>
2025-12-28 16:14:24 +0600

    Adjust BlogCategoryBadge positioning in BlogCard component for improved layout

--- modules/blogs/components/public/BlogCard.tsx
+++ modules/blogs/components/public/BlogCard.tsx
@@ -29,7 +29,7 @@ const BlogCard = ({ blog }: { blog: IBlog }) => {
           height={1000}
           className="object-cover"
         />
-        <BlogCategoryBadge className="absolute top-4 left-4" category={blog[BlogField.CATEGORY]} />
+        <BlogCategoryBadge className="absolute top-2 left-2" category={blog[BlogField.CATEGORY]} />
         <div className="p-4 grid grid-rows-[1fr_auto] gap-2">
           <h3 className="text-xl font-bold group-hover:text-accent line-clamp-2 leading-tight">
             {blog[BlogField.TITLE]}
