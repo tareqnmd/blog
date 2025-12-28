486ac441530d2bfefb9884f24f4bcda86a712857
Tareq <md.tareq@asthait.com>
2025-12-28 14:24:19 +0600

    Enhance BlogInfo component layout for better alignment and spacing
    - Updated the BlogInfo component to include justify-between for improved layout structure.
    - Removed unnecessary margin class from BlogShare component for cleaner styling.

--- modules/blogs/components/shared/BlogInfo.tsx
+++ modules/blogs/components/shared/BlogInfo.tsx
@@ -17,7 +17,7 @@ const BlogInfo = ({
   hideShare?: boolean;
 }) => {
   return (
-    <div className="flex items-center gap-8 text-sm">
+    <div className="flex items-center gap-8 text-sm justify-between">
       <div className="flex items-center gap-x-4 gap-y-2 flex-wrap text-muted">
         <div className="flex items-center gap-2">
           <div className="flex items-center gap-1">
@@ -54,7 +54,6 @@ const BlogInfo = ({
         <BlogShare
           title={blog[BlogField.TITLE]}
           link={`${process.env.NEXT_PUBLIC_BASE_URL}/blogs/${blog[BlogField.SLUG]}`}
-          className="ml-auto"
         />
       )}
     </div>
