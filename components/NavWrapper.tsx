a2e46beb595c202e31ce06b888a68446f8102430
Tareq <md.tareq@asthait.com>
2025-12-28 13:31:52 +0600

    Update Logo component to accept className prop for styling flexibility and adjust NavWrapper to apply text color to Logo

--- components/NavWrapper.tsx
+++ components/NavWrapper.tsx
@@ -10,7 +10,7 @@ const NavWrapper = async ({ children }: { children: React.ReactNode }) => {
   return (
     <header className="border-b border-border py-4 sticky top-0 z-50 bg-background/95 backdrop-blur">
       <nav className="container flex  flex-wrap items-center justify-between gap-6">
-        <Logo />
+        <Logo className="text-foreground" />
         <div className="flex items-center gap-4 flex-wrap">
           {children}
           {user ? (
